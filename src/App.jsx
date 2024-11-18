d33900fb1e0bbcd8dfef857adf764cbbe19882df
king-mayor <99553727+king-mayor@users.noreply.github.com>
2024-11-17 16:42:54 -0800

    updated transitioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { PacmanLoader } from "react-spinners";
+
 function App() {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     AOS.init();
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen w-full">
+          <PacmanLoader color="black" size={36} />
+        </div>
+      ) : (
+        <div>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
+        </div>
+      )}
+    </>
   );
 }
 
